b36a9bf6599909e2d21f99819f1b56c08651e1fb
FidelMaster <real-fidel1999@hotmail.com>
2020-04-20 15:36:56 -0600

    c

--- conexion.js
+++ conexion.js
@@ -5,7 +5,15 @@ module.exports = {
         host: 'localhost',
         user: 'root',
         password: '',
-        database: 'mombashop'
+        database: 'mombashop',
+     
     }
 
 };
+
+
+//host: '65.99.237.34',
+//user: 'fideljr_root',
+//password: 'fidelhernandez',
+//database: 'fideljr_mombashop',
+//port:3306 
