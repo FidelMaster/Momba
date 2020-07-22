b8a27deae65d16b4c4a059bc81dc71c9fffd6968
FidelMaster <45023881+FidelMaster@users.noreply.github.com>
2020-07-21 19:12:29 -0600

    Update conexion.js

--- conexion.js
+++ conexion.js
@@ -1,18 +1,12 @@
 module.exports = {
         database: {
-                connectionLimit: 10,
-                host: '143.95.239.47',
-                user: 'fridayni_admin',
-                password: 'admin2020',
-                database: 'fridayni_friday',
+                connectionLimit: max,
+                host: 'localhost',
+                user: 'root',
+                password: '',
+                database: 'Mombashop',
                 port: 3306
             }
          
 };
 
-
-//host: '65.99.237.34',
-//user: 'fideljr_root',
-//password: 'fidelhernandez',
-//database: 'fideljr_mombashop',
-//port:3306 
